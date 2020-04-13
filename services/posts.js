bc39bdc60fba9d96c5b3f47587b1fe48ea0cefc1
nguyinh <florent.dupontnguyen@gmail.com>
2020-04-13 18:41:30 +0200

    Posts back-end WIP

--- services/posts.js
+++ services/posts.js
@@ -1,18 +1,6 @@
 const mongoose = require("mongoose");
 const { Post } = require('../models');
 
-// exports.findAll = () => {
-//   return User.find({});
-// };
-
-// exports.findById = _id => {
-//   return User.findOne({_id});
-// };
-
-// exports.findByEmail = email => {
-//   return User.findOne({email});
-// };
-
 exports.add = (author, shopPicture, articles) => {
   return Post.create({
     _id: new mongoose.Types.ObjectId(),
