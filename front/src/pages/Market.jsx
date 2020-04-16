896f85f7c2b5cfafc7d1519cdaaf306ea2302853
nguyinh <florent.dupontnguyen@gmail.com>
2020-04-16 15:05:35 +0200

    Securized creator inputs

--- front/src/pages/Market.jsx
+++ front/src/pages/Market.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState, useContext } from "react";
 import { AppContext } from "../contexts";
 import { getPosts } from "../services";
-import { Button, Form, Header, Divider } from "semantic-ui-react";
+import {
+  Button,
+  Form,
+  Header,
+  Divider,
+  Segment,
+  Dimmer,
+  Loader,
+  Image,
+} from "semantic-ui-react";
 import PostCreator from "./PostCreator";
 
 const days = new Array(
@@ -30,17 +39,20 @@ const months = new Array(
 
 const Market = () => {
   const [isCreating, setIsCreating] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [posts, setPosts] = useState([]);
 
   const fetchPosts = async () => {
+    setIsLoading(true);
     let posts = await getPosts();
-    console.log(posts);
+
     posts = posts.map((post) => ({
       ...post,
       shopPictureSrc: Buffer.from(post.shopPicture.data, "base64"),
     }));
 
     setPosts(posts);
+    setIsLoading(false);
   };
 
   const formatDate = (date) => {
@@ -57,7 +69,7 @@ const Market = () => {
     <>
       {" "}
       {isCreating ? (
-        <PostCreator />
+        <PostCreator setIsCreating={setIsCreating} />
       ) : (
         <div className="market">
           <div className="market--header">
@@ -72,31 +84,46 @@ const Market = () => {
               + Créer annonce
             </Button>
           </div>
-          {!!posts.length &&
-            posts.map((post) => (
-              <>
-                <Header as="h3">{post.author.pseudo}</Header>
+          {isLoading ? (
+            <Loader
+              active
+              inline="centered"
+              size="big"
+              style={{ marginTop: "5rem" }}
+            >
+              Chargement des annonces...
+            </Loader>
+          ) : (
+            <>
+              {!!posts.length &&
+                posts.map((post) => (
+                  <>
+                    <Header as="h3">{post.author.pseudo}</Header>
 
-                <div className="market--post--shop-picture">
-                  <img src={post.shopPictureSrc} className="shop-picture" />
-                </div>
-
-                {post.items.map(({ name, price }) => (
-                  <div className="market-items--input">
-                    <div className="market-items--post--item">
-                      <span className="market-items--creator--item-name">
-                        {name}
-                      </span>
-                      <span className="market-items--creator--item-price">{`${price}$`}</span>
+                    <div className="market--post--shop-picture">
+                      <img src={post.shopPictureSrc} className="shop-picture" />
                     </div>
-                    <Button color="teal" compact onClick={null}>
-                      👈 I want it
-                    </Button>
-                  </div>
+
+                    {post.items.map(({ name, price }) => (
+                      <div className="market-items--input">
+                        <div className="market-items--post--item">
+                          <span className="market-items--creator--item-name">
+                            {name}
+                          </span>
+                          {price && (
+                            <span className="market-items--creator--item-price">{`${price}$`}</span>
+                          )}
+                        </div>
+                        <Button color="teal" compact onClick={null}>
+                          👈 I want it
+                        </Button>
+                      </div>
+                    ))}
+                    <Divider style={{ margin: "3rem 3rem 2rem" }} />
+                  </>
                 ))}
-                <Divider style={{margin: '3rem 3rem 2rem'}}/>
-              </>
-            ))}
+            </>
+          )}
         </div>
       )}
     </>
--- front/src/pages/PostCreator.jsx
+++ front/src/pages/PostCreator.jsx
@@ -1,26 +1,54 @@
-import React, { useEffect, useState, useRef } from "react";
-import { AppContext } from "../contexts";
-import { createPost, getPosts } from "../services";
+import React, { useState, useRef } from "react";
+import { createPost } from "../services";
 import { Button, Segment, Header, Icon, Input, Label } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
 
-const PostCreator = () => {
+const PostCreator = ({ setIsCreating }) => {
   const [shopPicture, setShopPicture] = useState(null);
   const [items, setItems] = useState([]);
   const [itemName, setItemName] = useState("");
   const [itemPrice, setItemPrice] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [publishError, setPublishError] = useState(false);
+  const [isPublishing, setIsPublishing] = useState(false);
+  const [isPublished, setIsPublished] = useState(false);
   const inputRef = useRef(null);
 
+  const addItemPrice = (price) => {
+    const digitRegex = /^\d+$/;
+
+    const value = price.trim();
+
+    if (!digitRegex.test(value) && value !== '') return;
+
+    setItemPrice(value);
+  };
 
   const addItemToList = () => {
+    if (itemName === "") {
+      setNameError("Frère tu forces, indique au moins le nom de l'article");
+      return;
+    }
+
     setItems([...items, { name: itemName, price: itemPrice }]);
     setItemName("");
     setItemPrice("");
+    setNameError("");
   };
 
   const publish = async () => {
     if (itemName && itemPrice) addItemToList();
+    if (!shopPicture) {
+      setPublishError(true);
+      return;
+    }
 
+    setIsPublishing(true);
     await createPost(items, new Buffer(shopPicture, "utf8"));
+    setIsPublishing(false);
+    setIsPublished(true);
+
+    setTimeout(() => setIsCreating(false), 1000);
   };
 
   const simulateInputClick = () => {
@@ -33,20 +61,6 @@ const PostCreator = () => {
 
     reader.onload = async () => {
       setShopPicture(reader.result);
-
-      // Convert to a stockable format
-      const buf = new Buffer(reader.result, "utf8");
-
-      // try {
-      //   const test = await axios.post("/api/posts", {
-      //     _id: user._id,
-      //     articles: [],
-      //     shopPicture: buf,
-      //   });
-      //   console.log(test);
-      // } catch (err) {
-      //   console.log(err);
-      // }
     };
 
     reader.readAsDataURL(file);
@@ -70,6 +84,7 @@ const PostCreator = () => {
           placeholder
           onClick={simulateInputClick}
           className="shop-picture--input"
+          style={{ backgroundColor: publishError && "#fff6f5" }}
         >
           <Header icon>
             <Icon name="plus" />
@@ -85,7 +100,9 @@ const PostCreator = () => {
           <div className="market-items--input">
             <div className="market-items--creator--item">
               <span className="market-items--creator--item-name">{name}</span>
-              <span className="market-items--creator--item-price">{`${price}$`}</span>
+              {price && (
+                <span className="market-items--creator--item-price">{`${price}$`}</span>
+              )}
             </div>
             <Button
               icon
@@ -98,35 +115,45 @@ const PostCreator = () => {
             </Button>
           </div>
         ))}
-        <div className="market-items--input">
-          <Input
-            icon="tag"
-            iconPosition="left"
-            placeholder="Nom article"
-            value={itemName}
-            onChange={(_, { value }) => setItemName(value)}
-          />
-          <Input
-            icon="dollar"
-            iconPosition="left"
-            placeholder="Prix"
-            className="market-items--price-input"
-            value={itemPrice}
-            onChange={(_, { value }) => setItemPrice(value)}
-          />
-          <Button icon color="olive" onClick={addItemToList}>
-            <Icon name="plus" />
-          </Button>
+
+        <div className="market-items--input-with-error">
+          <div className="market-items--input">
+            <Input
+              icon="tag"
+              iconPosition="left"
+              placeholder="Nom article"
+              value={itemName}
+              error={nameError}
+              onChange={(_, { value }) => setItemName(value)}
+            />
+            <Input
+              icon="dollar"
+              iconPosition="left"
+              placeholder="Prix"
+              className="market-items--price-input"
+              value={itemPrice}
+              onChange={(_, { value }) => addItemPrice(value)}
+            />
+            <Button icon color="olive" onClick={addItemToList}>
+              <Icon name="plus" />
+            </Button>
+          </div>
+          {nameError && <span>{nameError}</span>}
         </div>
 
         <Button
-          content="Publier"
+          icon={isPublished}
           fluid
           primary
           size="huge"
+          color={isPublished ? "green" : "blue"}
           className="market-items--publish"
           onClick={publish}
-        />
+          loading={isPublishing}
+          disabled={isPublishing || isPublished}
+        >
+          {isPublished ? <Icon name="check" /> : "Publier"}
+        </Button>
       </div>
     </div>
   );
