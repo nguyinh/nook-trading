bab2d337d9ad8b0bf304135e0c0beaeb44ddcc3f
nguyinh <florent.dupontnguyen@gmail.com>
2020-04-30 23:48:15 +0200

    Log if trend created

--- middlewares/createTrend.js
+++ middlewares/createTrend.js
@@ -1,6 +1,7 @@
 const Boom = require("@hapi/boom");
 require("dotenv").config();
 const { turnips } = require("../services");
+const logger = require('./logger');
 
 module.exports = async (req, res, next) => {
   try {
@@ -10,6 +11,8 @@ module.exports = async (req, res, next) => {
     const latestTrend = await turnips.findLatestTrend(_id, thisSunday);
 
     if (!latestTrend) {
+      logger.info(`[MIDDLEWARES] createTrend | ${_id}`);
+
       const trend = await turnips.add(_id);
       req.trend = trend;
     }
