a5c0979ba28e446136e7324145b344c888767d1b
nguyinh <florent.dupontnguyen@gmail.com>
2020-05-07 15:05:28 +0200

    Create a trend on fetching all week trends

--- routes/turnips.js
+++ routes/turnips.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { turnips } = require('../controllers');
-const { verifyJWT, findUser, createTrend } = require('../middlewares');
+const { turnips } = require("../controllers");
+const { verifyJWT, findUser, createTrend } = require("../middlewares");
 
 router
-  .route('/turnipTrends')
-  .get(verifyJWT, turnips.get)
+  .route("/turnipTrends")
+  .get(verifyJWT, findUser, createTrend, turnips.get)
   .post(verifyJWT, findUser, turnips.create);
 
 router
-  .route('/turnipTrends/prices')
+  .route("/turnipTrends/prices")
   .get(verifyJWT, turnips.getPrices)
   .post(verifyJWT, findUser, createTrend, turnips.createPrice)
   .put(verifyJWT, findUser, createTrend, turnips.updatePrices);
 
 router
-  .route('/turnipTrends/ownedQuantity')
+  .route("/turnipTrends/ownedQuantity")
   .post(verifyJWT, findUser, createTrend, turnips.updateOwnedQuantity);
 
 router
-  .route('/turnipTrends/ownedPrice')
+  .route("/turnipTrends/ownedPrice")
   .post(verifyJWT, findUser, createTrend, turnips.updateOwnedPrice);
 
 router
-  .route('/turnipTrends/prices/sunday')
+  .route("/turnipTrends/prices/sunday")
   .post(verifyJWT, findUser, createTrend, turnips.createSundayPrice);
 
 module.exports = router;
