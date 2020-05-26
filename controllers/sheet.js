c7f579a9a8ed2573283c660ca6cfdeeda11ab461
Jack K <jack.karuneswaran@gmail.com>
2020-05-26 22:59:27 +0200

    v4 API sheet

--- controllers/sheet.js
+++ controllers/sheet.js
@@ -1,11 +1,11 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
 
-const cred = require('../models/client_secret.json');
+const cred = require('../client_secret.json');
 
 
 
-exports.TEST = async (req, res, next) => {
+/* exports.TEST = async (req, res, next) => {
     try {
         const doc = new GoogleSpreadsheet('1BjqVeqIrfEezvyrWLUrwMjmK_UbY2LXkZ12mttamTtk');
 
@@ -26,5 +26,39 @@ exports.TEST = async (req, res, next) => {
       return res.send();
     } catch (err) {
       return next(err);
+  }
+}; */
+
+exports.TEST = async (req, res, next) => {
+
+  const { google } = require('googleapis');
+  const keys = require('../client_secret.json');
+
+  const client = new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  );
+
+  // TODO AUTHORIZE ONLY ONCE
+  client.authorize(async (err, tokens) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("SUCCES");
+      const gsapi = google.sheets({ version: 'v4', auth: client });
+    
+      const opt = {
+        spreadsheetId: '1BjqVeqIrfEezvyrWLUrwMjmK_UbY2LXkZ12mttamTtk',
+        range: 'Rugs!H:H'
+      }
+    
+      let data = await gsapi.spreadsheets.values.get(opt);
+      const allRugs = [...data.data.values];
+      console.log(allRugs.filter((rug) => rug.toString().startsWith('tapis T')));
     }
-  };
+  });
+
+
+};
