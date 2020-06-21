dd21c32d3765f91e9e50b07d64aab9cb402b23bb
nguyinh <florent.dupontnguyen@gmail.com>
2020-06-21 12:30:46 +0200

    Best pair programming ever

--- front/src/pages/PostCreator.jsx
+++ front/src/pages/PostCreator.jsx
@@ -2,10 +2,11 @@ import React, { useState, useRef, useEffect } from "react";
 import { createPost, getNames, getALL } from "../services";
 import { Button, Segment, Header, Icon, Input, Label } from "semantic-ui-react";
 import bellsImage from "../res/images/bells-2.png";
-import Compressor from 'compressorjs';
-import { Search, Divider } from 'semantic-ui-react'
-const initialState = { isLoading: false, results: [], value: '' }
-var _ = require('lodash');
+import { ReactComponent as LeafIcon } from "../res/images/leaf-icon.svg";
+import Compressor from "compressorjs";
+import { Search, Divider } from "semantic-ui-react";
+const initialState = { isLoading: false, results: [], value: "" };
+var _ = require("lodash");
 
 const PostCreator = ({ backFromCreator }) => {
   const [shopPicture, setShopPicture] = useState(null);
@@ -13,6 +14,7 @@ const PostCreator = ({ backFromCreator }) => {
   const [itemName, setItemName] = useState("");
   const [itemPrice, setItemPrice] = useState("");
   const [itemImage, setItemImage] = useState("");
+  const [isCustom, setIsCustom] = useState(false);
   const [nameError, setNameError] = useState("");
   const [publishError, setPublishError] = useState(false);
   const [isPublishing, setIsPublishing] = useState(false);
@@ -20,7 +22,7 @@ const PostCreator = ({ backFromCreator }) => {
   const inputRef = useRef(null);
   const nameInputRef = useRef(null);
   const priceInputRef = useRef(null);
-  const [options, setOptions] = useState([]); 
+  const [options, setOptions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [value, setValue] = useState("");
   const [results, setResults] = useState([]);
@@ -33,7 +35,7 @@ const PostCreator = ({ backFromCreator }) => {
     if (!digitRegex.test(value) && value !== "") return;
 
     setItemPrice(value);
-    addItemToList();
+    // addItemToList();
   };
 
   const addItemToList = () => {
@@ -46,7 +48,14 @@ const PostCreator = ({ backFromCreator }) => {
       return;
     }
 
-    setItems([...items, { name: itemName, price: itemPrice && parseInt(itemPrice) , img: itemImage}]);
+    setItems([
+      ...items,
+      {
+        name: itemName,
+        price: itemPrice && parseInt(itemPrice),
+        img: itemImage,
+      },
+    ]);
     setItemName("");
     setItemPrice("");
     setNameError("");
@@ -79,7 +88,7 @@ const PostCreator = ({ backFromCreator }) => {
       maxHeight: 500,
       convertSize: 100000, // 100 kB
       quality: 0.8,
-      success: blob => {
+      success: (blob) => {
         const reader = new FileReader();
 
         reader.onload = async () => {
@@ -88,9 +97,9 @@ const PostCreator = ({ backFromCreator }) => {
 
         reader.readAsDataURL(blob);
       },
-      error: err => {
+      error: (err) => {
         console.error(err.message);
-      }
+      },
     });
   };
 
@@ -98,29 +107,49 @@ const PostCreator = ({ backFromCreator }) => {
     const formattedList = [];
     // Ask to server autocompleting datas
     getALL().then((data) => {
-      data = data.filter((value) => value.french)
+      data = data.filter((value) => value.french);
       data.forEach((entry) => {
         let computedID = entry.id;
         if (entry.idVariant && entry.idVariant !== "NA") {
-          computedID += entry.idVariant
+          computedID += entry.idVariant;
         }
         if (entry.french) {
-          formattedList.push({key: computedID, value: computedID, title: entry.french, image: entry.image, price: entry.sell.toString() })
+          formattedList.push({
+            key: computedID,
+            value: computedID,
+            title: entry.french,
+            image: entry.image,
+            price: entry.sell.toString(),
+          });
         }
       });
       setOptions(formattedList);
     });
-  }
+  };
 
   useEffect(() => {
-    setSearchOptions()
+    setSearchOptions();
   }, []);
 
   const handleResultSelect = (e, { result }) => {
     // setItemName(result.title);
     // setItemImage(result.image);
     // addItemPrice(result.price);
-    setItems([...items, { name: result.title, price: result.price && parseInt(result.price) , img: result.image}]);
+    console.log(result);
+    if (!result.image && !result.price) {
+      setItemName(result.title);
+      setIsCustom(true);
+      // addItemPrice(result.price);
+    } else {
+      setItems([
+        ...items,
+        {
+          name: result.title,
+          price: result.price && parseInt(result.price),
+          img: result.image,
+        },
+      ]);
+    }
   };
 
   const handleSearchChange = (e, { value }) => {
@@ -129,18 +158,22 @@ const PostCreator = ({ backFromCreator }) => {
 
     setTimeout(() => {
       if (!value) {
-        setIsLoading(initialState.isLoading); 
-        setValue(initialState.value); 
+        setIsLoading(initialState.isLoading);
+        setValue(initialState.value);
         setResults(initialState.results);
         return;
       }
       setIsLoading(false);
-      setResults(options.filter((entry) => entry.title.startsWith(value)).slice(0, 20));
-    }, 300)
-  }
+      const filteredOptions = options
+        .filter((entry) => entry.title.startsWith(value))
+        .slice(0, 20);
 
+      setResults(
+        filteredOptions.length ? filteredOptions : [{ id: 0, title: value }]
+      );
+    }, 300);
+  };
 
-  
   return (
     <div className="market--post-creator">
       <Header as="h2">Ton annonce du jour</Header>
@@ -174,23 +207,24 @@ const PostCreator = ({ backFromCreator }) => {
         </Segment>
       )}
 
-
       <div className="market-items--creator">
         <Header as="h3">Ajoute tes items</Header>
-        
-        
+
         {items.map(({ name, price, img }) => (
           <div className="market-items--input">
             <div className="market-items--creator--item">
-              <span><img src={img}className='market-items--bell-image'/></span>
+              <span>
+                {img ? (
+                  <img src={img} className="market-items--bell-image" />
+                ) : (
+                  <LeafIcon />
+                )}
+              </span>
               <span className="market-items--creator--item-name">{name}</span>
               {price && (
                 <>
                   <span className="market-items--creator--item-price">{`${price}`}</span>
-                  <img
-                    src={bellsImage}
-                    className='market-items--bell-image'
-                  />
+                  <img src={bellsImage} className="market-items--bell-image" />
                 </>
               )}
             </div>
@@ -206,35 +240,39 @@ const PostCreator = ({ backFromCreator }) => {
           </div>
         ))}
 
-        <div className="market-items--input-with-error">
-          <div className="market-items--input">
+        {isCustom && (
+          <div className="market-items--input-with-error">
+            <div className="market-items--input">
+              <Label icon="tag" iconPosition="left" placeholder="Nom item">
+                <Icon name="tag"></Icon>
+                {itemName}
+              </Label>
 
-            <Label
-              icon="tag"
-              iconPosition="left"
-              placeholder="Nom item"
-            ><Icon name="tag"></Icon>{itemName}</Label>
-            
-            <Label
-              icon="dollar"
-              iconPosition="left"
-              placeholder="Prix"
-              className="market-items--price-input"
-            ><Icon name= "dollar"></Icon>{itemPrice}</Label>
+              <Input
+                icon="dollar"
+                iconPosition="left"
+                placeholder="Prix"
+                className="market-items--price-input"
+                value={itemPrice}
+                onKeyDown={(e) => e.key === "Enter" && addItemToList()}
+                ref={priceInputRef}
+                onChange={(_, { value }) => addItemPrice(value)}
+              />
 
-            <Button icon color="olive" onClick={addItemToList}>
-              <Icon name="plus" />
-            </Button>
+              <Button icon color="olive" onClick={addItemToList}>
+                <Icon name="plus" />
+              </Button>
+            </div>
+            {nameError && <span>{nameError}</span>}
           </div>
-          {nameError && <span>{nameError}</span>}
-        </div>
+        )}
 
         <Divider />
 
         <Search
           loading={isLoading}
           fluid
-          placeholder= {`Cherche ton item ici`}
+          placeholder={`Cherche ton item ici`}
           minCharacters={3}
           size={"massive"}
           onResultSelect={handleResultSelect}
@@ -243,7 +281,7 @@ const PostCreator = ({ backFromCreator }) => {
           })}
           results={results}
           value={value}
-      />
+        />
 
         <Button
           icon={isPublished}
